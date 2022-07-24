e3c2c72c956ea8e3aa3bf87f44d652306e381040
Roger Massana <rogermassana2002@gmail.com>
2022-07-24 19:12:22 +0200

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || !(typeof a === 'number') || !(typeof b === 'number') )
+        return 'ERROR';
+    if (a > b) {
+        const aux = b;
+        b = a;
+        a = aux;
+    }
+    for (; a <= b; a++) {
+        sum += a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
