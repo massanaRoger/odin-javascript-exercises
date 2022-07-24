5c359741d3ed4266e5b6ee57f1490ece65c9a0db
Roger Massana <rogermassana2002@gmail.com>
2022-07-24 18:36:54 +0200

    Do exercises 1, 2, 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n === 0) {
+        return "";
+    }
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let strResult = string;
+    for (let i = 0; i < n - 1 ; i++) {
+        strResult += string;
+    }
+    return strResult;
 };
 
 // Do not edit below this line
