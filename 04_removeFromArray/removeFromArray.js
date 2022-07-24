871abde0d0663e65675e268c2288fd69b2b5fc11
Roger Massana <rogermassana2002@gmail.com>
2022-07-24 19:01:28 +0200

    Do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    const indexes = [];
+    elements.forEach(el => {
+        indexes.push(array.indexOf(el))
+    })
+    indexes.sort().reverse();
+    for (const index of indexes) {
+        if (index !== -1)
+            array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
