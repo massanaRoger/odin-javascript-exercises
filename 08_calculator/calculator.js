c62081cee5d7e66acb095922b60c62d594534764
Roger Massana <rogermassana2002@gmail.com>
2022-07-26 18:54:47 +0200

    Do exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return +(args.reduce((sum, number) => {
+        return sum + number;
+    }, 0))
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+    return args.reduce((sum, number) => {
+      return sum * number;
+    }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0 || number === 1) return 1;
+    return factorial(number - 1) * number;
 };
 
 // Do not edit below this line
